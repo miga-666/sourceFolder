c4658c54cd4d97f97c1b45bf63b27c8ab01fc900
tsai-jen <s108213029@mail1.ncnu.edu.tw>
2022-04-04 16:39:13 +0800

    4/4

--- c/3.30.c
+++ c/3.30.c
@@ -6,19 +6,17 @@
 
 int value = 5;
 
-int main() 
-{
-pid_t pid;
-
+int main() {
+    pid_t pid;
     pid = fork();
 
     if(pid == 0) { /*child process*/
         value += 15;
         return 0;
-    }
-    else if (pid > 0) { /*parent process*/
+    } 
+    else if(pid > 0) { /*parent process*/
         wait(NULL);
-        printf("PARENT: value = %d", value); /*LINE A*/
+        printf("PARENT: value = %d", value);
         return 0;
     }
 }
